99dbc32c67920c11629b6859182d7fa3824ed0b8
pallis-sotiris-39 <el17810@mail.ntua.gr>
2021-04-21 16:10:19 +0300

    make question man

---
+++ SoA/Backend/question-man/src/question/question.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateQuestionDto } from './dto/create-question.dto';
+import { UpdateQuestionDto } from './dto/update-question.dto';
+import { Question } from './entities/question.entity';
+
+@Injectable()
+export class QuestionService {
+  private readonly questions: Question[] = [];
+
+  create(createQuestionDto: CreateQuestionDto) {
+    const id = this.questions.length + 1;
+    const quest = { id: id, ...createQuestionDto };
+    this.questions.push(quest);
+    return quest;
+  }
+
+  findAll() {
+    return this.questions;
+  }
+
+  findOne(id: number) {
+    const quest = this.questions.find((quest) => quest.id == id);
+    if (!quest) throw new NotFoundException('Question ${id} not found.');
+    return quest;
+  }
+
+  update(id: number, updateQuestionDto: UpdateQuestionDto) {
+    return `This action updates a #${id} question`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} question`;
+  }
+}
