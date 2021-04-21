d043740452c9b3522a64d98e58aab00d56106aac
pallis-sotiris-39 <el17810@mail.ntua.gr>
2021-04-21 14:45:07 +0300

    Revert "Revert "make quiz man""
    This reverts commit f2bd184e448389c933a6825a7a6035034b704a91.

--- SoA/Backend/Authentication/routes/index.js
+++ SoA/Backend/Authentication/routes/index.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const jwt = require('jsonwebtoken')
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const JWT_SECRET = 'our-biggest-secret'
+
 passport.use('signin', new LocalStrategy(function (username, password, done){
   if ( username !== 'typo' || password !== 'something'){
     return done(null, false);
@@ -12,14 +17,33 @@ passport.use('signin', new LocalStrategy(function (username, password, done){
   return done(null, {username: username});
 }));
 
+passport.use('token', new JWTStrategy(
+    {
+        secretOrKey: JWT_SECRET,
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken()
+    },
+    function(token,done){
+        return done(null, {username: token.username})
+    }
+))
+
 // POST signin
 router.post('/signin',
     passport.authenticate('signin', {session: false}),
     function(req, res, next) {
-    res.json({
-      user: req.user,
-      timestamp: Date.now()
-    });
-});
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.json({
+            token: jwt.sign(req.user, JWT_SECRET, { expiresIn: 3600 })
+        });
+    }
+);
+
+//GET whoami
+router.get('/whoami',
+    passport.authenticate('token', { session: false}),
+    function (req,res, next) {
+        res.json({user: req.user});
+    }
+);
 
 module.exports = router;
