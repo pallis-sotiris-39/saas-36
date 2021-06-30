21df6c9f9cd95d4a1e61d78363034a9210ae936d
Sotirios Pallis <el17810@mail.ntua.gr>
2021-06-30 22:37:19 +0300

    Fix config files

--- SoA/Backend/Authentication/bin/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-const connectionString = `postgresql://typo:password@localhost:5432/saas-36`;
-
-const pool = new Pool({
-    connectionString: connectionString
-});
-
-module.exports = {pool};
